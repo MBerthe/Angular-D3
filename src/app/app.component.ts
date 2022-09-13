72f8fce04ffc8d9e826cc55c7bf4b2dd4ec1ee29
MBerthe <35028407+MBerthe@users.noreply.github.com>
2022-09-13 23:55:26 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,41 @@ import * as d3 from 'd3';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-    let svg = d3.select("#svg1").append("svg");
+    const backgroundArc = d3
+      .arc()
+      .innerRadius(30)
+      .outerRadius(50)
+      .startAngle(0)
+      .endAngle(Math.PI * 2);
 
+    const mainArc = d3
+      .arc()
+      .innerRadius(30)
+      .outerRadius(50)
+      .cornerRadius(10)
+      .startAngle(0)
+      .endAngle((d: number) => (d / 100) * Math.PI * 2);
+
+    const data = [30]; // percents.
+
+    const svg = d3
+      .select('body')
+      .append('svg')
+      .attr('width', 100)
+      .attr('height', 200);
+
+    const charts = svg
+      .selectAll('g')
+      .data(data)
+      .enter()
+      .append('g')
+      .attr(
+        'transform',
+        (d: number, i: number) => 'translate(' + (50) + ',100)'
+      );
+
+    charts.append('path').attr('d', backgroundArc).attr('fill', '#ccc');
+
+    charts.append('path').attr('d', mainArc).attr('fill', 'orange');
   }
 }
